ea9629ea82865c1f379bb568413e58924b7a6bc6
Anwesha Dutta <anweshadutta891@gmail.com>
2024-03-10 19:22:09 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnAboutView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //Action Mehod
+        public ActionResult Index()
+        {
+            //return View("Rename");
+            return View("~/Views/MyView/MyNewView.cshtml");
+        }
+
+        //Action Method
+        public ViewResult AboutUS()
+        {
+            return View();
+        }
+
+        public ActionResult WithoutView()
+        {
+            return View();
+        }
+    }
+}
